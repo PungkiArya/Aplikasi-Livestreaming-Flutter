0dcad4950b73ab74e9b713ee12a7b5a5914e3670
PungkiArya <aryatamapungki@gmail.com>
2020-11-23 21:14:37 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,46 @@ import 'package:provider/provider.dart';
 import './models/video_model.dart';
 import './screens/video_detail.dart';
 import './screens/video_list.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      title: new Text(
+        'Welcome In SplashScreen',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      seconds: 8,
+      navigateAfterSeconds: AfterSplash(),
+      image: new Image.asset('assets/welcome.gif'),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 150.0,
+      onClick: () => print("Flutter Egypt"),
+      loaderColor: Colors.white,
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        // ignore: missing_required_param
         ChangeNotifierProvider(
           builder: (_) => VideoProvider(),
         )
